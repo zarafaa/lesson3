01218fc2b70802ef38b516437995f1a24d8af4f4
zarafaa <annzhen2003@gmail.com>
2019-02-16 14:07:14 +0200

    3 final

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var array = ["Пушок","кошка","белка"]
+array.pop();
+console.log(array);
+array.push("кит","слон","яблоко",77, true, [ "String1","String2"]);
+console.log(array);
+array.unshift("слон", "яблоко", false)
+console.log(array);
+
+
+
+
+// var text = "у меня есть "
+// for (var i = array.length - 1; i >= 0; i--) {
+// 	if (array[i] == "белка") {
+// 		console.log(`${text} ${array[i]}` );
+// 	}
+// 	if(array[i] == "кошка"){
+// 	console.log(`${text} ${array[i]} мурка` );
+// }
+
+
+
